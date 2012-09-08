81503f3105a03bc87ca5295a548a8ac4d0f0306d
anna mendoza <anna@curvecommunications.biz>
2012-09-08 13:01:22 -0700

    NEW WEBSITE
    and Lars helped name it!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wondergem::Application.config.secret_token = '5d7977ad86a8da1d985a4b2cc1676cf29c5f4d5932c6f76556c2a7208af548e1a01f870efae8f7e524686abc43d1e13fee8a0ff92d70592addf0c249643f5d37'
