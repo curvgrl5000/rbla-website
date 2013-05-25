1395ae2ed6974638d9fba04e525810531eed5062
anna mendoza <anna@curvecommunications.biz>
2013-05-25 00:32:38 -0700

    updated all the files with the new module markup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
-end
+  def accordion_block(title, slug, parent, &block)
+      content_tag(:div, :class => "accordion-group") do
+        head = content_tag(:div, :class => "accordion-heading") do
+          link_to(title, "#collapse_#{slug}", :class => "accordion-toggle", :"data-toggle" => "collapse", :"data-parent" => "##{parent}")
+        end
+        
+        body = content_tag(:div, :id => "collapse_#{slug}", :class => "accordion-body collapse") do
+          content_tag(:div, :class => "accordion-inner") do
+            yield
+          end
+        end
+        head + body
+      end
+    end
+  end
